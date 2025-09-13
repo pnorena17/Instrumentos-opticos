0ad6808d37353697f085e5e063146a1a8780e3ab
LucasOsorno <losornoo@unal.edu.co>
2025-09-13 12:08:12 -0500

    Declaración algunas variables

--- practica_1/punto1/punto1.py
+++ practica_1/punto1/punto1.py
@@ -1,4 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-a= 3
+N_f= 0.25 #Numero de Frsnel arbitrario(ej del libro)
+M = 5*N_f #Criterio de M > aN_f
+Q = 130 #debe ser mayor a q y depende de el orden de interpolacion
+N = Q*M #Numero de muestras totales ?
+
