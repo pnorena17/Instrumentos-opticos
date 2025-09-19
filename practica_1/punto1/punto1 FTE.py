eb0b9c210bd0aa165efa50da2b96624b61e85c50
LucasOsorno <losornoo@unal.edu.co>
2025-09-18 20:54:23 -0500

    correcciones

--- practica_1/punto1/punto1 FTE.py
+++ practica_1/punto1/punto1 FTE.py
@@ -1,65 +1,96 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#En primer lugar, definimos la abertura y longitud de onda
+#En primer lugar, definimos la abertura y longitud de 
+
+
 long_de_onda = 650*(10**(-9)) #(en metros) Usamos la longitud de onda del  rojo: 650 nm
-#Por decir, usaremos de abertura un cuadrado de lado l
-l = 1*(10**(-3)) #(en metros) Usamos dimesión máxima: 10 cm
-z = 1
+N = 1080 #pixeles de la camara
+dx = 2.9*(10**(-6)) # Espaciado en el dominio espacial 2,9um(cuadrada)
+L = N*dx #Dimensiones del sensor
+
+l = 0.0001 #(diemnsiones de la apertura cuadrada) Usamos dimensión máxima: 1 cm
+M = int((l/L)*N)
+
+df = 1/L
+
 
 ##################  MÉTODO POR FUNCION DE TRANSFERENCIA EXACTA    ######################################################33
 
-#Creamos las variables
-M = 256
 
-if M%2 == 0:
-    N = 8*M
-else:
-    N = 8*M +1 
+#if M%2 == 0:
+ #   N = 12*M
+#else:
+#    N = 12*M +1 
+     # Espaciado en el dominio de la frecuencia (cuadrada)
+     
+z = 0.04
+z_max = M*(dx**2)/long_de_onda
+print(z_max, M)
+#assert z <= z_max, "No cumple el criterio de z para TF"
+
+
     
 abertura = np.ones((M,M), dtype=complex)
 padded_array = np.zeros((N, N), dtype=complex)
 min_index = (N-M)//2
 padded_array[min_index : min_index + M, min_index : min_index + M] = abertura
 
-dx = l/M # Tamaño del píxel en la apertura
-L=l*N/M # Tamaño total de la cuadrícula computacional
+# Discretizacion dominio espacial
 
-x = (np.arange(N) - N/2) * dx
-y = (np.arange(N) - N/2) * dx
-X, Y = np.meshgrid(x, y)
+n = (np.arange(N) - N/2)
+m = (np.arange(N) - N/2)
 
-df_x = 1/L # Espaciado en el dominio de la frecuencia
-p = (np.arange(N) - N/2) * df_x
-q = (np.arange(N) - N/2) * df_x
+x = n*dx
+y = m*dx
+
+X, Y = np.meshgrid(n, m)
+
+# Discretizacion espectro
+ 
+p = (np.arange(N) - N/2) 
+q = (np.arange(N) - N/2) 
+
+fx = p*df
+fy = q*df
 P, Q = np.meshgrid(p, q)
 
-f_max = M/L #Criterio de Aliasing
+
+k = 2*np.pi/long_de_onda #numero de onda
+
+#Calculamos la matriz de fase cuadrática
+
+#Multiplicación campo de entrada por la fase de entrada
+campo_en_apertura = padded_array     
+
+f_max = M*df #Criterio de Aliasing
 
 #Transformada de Fourier
-difraccion_fft = np.fft.fft2(padded_array)
-centrar_fft = np.fft.fftshift(difraccion_fft)
+difraccion_fft = np.fft.fft2(campo_en_apertura)* dx**2
+centrar_fft = dx**2 * np.fft.fftshift(difraccion_fft)
 
 #Manejo ondas evanescentes
-argumento_raiz = 1 - (long_de_onda**2) * (P**2 + Q**2)
+argumento_raiz = 1 - (((long_de_onda*df)**2) * (P**2 + Q**2))
+print(1/long_de_onda)
+print(M*df)
 # Filtro para mantener solo las frecuencias que se propagan (argumento >= 0)
 filtro_propagacion = (argumento_raiz >= 0)
 
 # Inicializamos H con ceros (complejos)
 FuncionTransfer = np.zeros((N, N), dtype=complex)
 # Calculamos H solo para las frecuencias que se propagan
-FuncionTransfer[filtro_propagacion] = np.exp(1j*2*np.pi*z/long_de_onda * np.sqrt(argumento_raiz[filtro_propagacion]))
+FuncionTransfer[filtro_propagacion] = np.exp(1j*z*k*np.sqrt(argumento_raiz[filtro_propagacion]))
 
 
 A = centrar_fft * FuncionTransfer
 shift_A = np.fft.ifftshift(A)   #shift preparado para hacer la inversa
-CampoSalida = np.fft.ifft2(shift_A) #inversa de fourier
+CampoSalida = df**2 * np.fft.ifft2(shift_A) #inversa de fourier
 
 
 intensidad = abs(CampoSalida)**2
 max_intensidad = np.max(intensidad)
 if max_intensidad > 0:
-    intensidad_log = np.log1p(intensidad / max_intensidad * 1000)
+    intensidad_log = np.log1p(intensidad / max_intensidad * 100)
     intensidad_norm = intensidad_log / np.max(intensidad_log)
 else:
     intensidad_norm = intensidad
