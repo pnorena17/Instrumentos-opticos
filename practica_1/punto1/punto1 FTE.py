2d5b64f2b459ad29faa16c869ac9cea1a00da737
LucasOsorno <losornoo@unal.edu.co>
2025-09-20 19:36:56 -0500

    correcciones

--- practica_1/punto1/punto1 FTE.py
+++ practica_1/punto1/punto1 FTE.py
@@ -1,99 +1,83 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#En primer lugar, definimos la abertura y longitud de 
+####Creamos las Variables
+
+long_onda = 633e-9
+#long_onda = long_onda_nm * 1e-6
+k = (2*np.pi)/long_onda
 
 
-long_de_onda = 633*(10**(-9)) #(en metros) Usamos la longitud de onda del  rojo: 650 nm
-N = 1080 #pixeles de la camara
-dx = 6.9*(10**(-6)) # Espaciado en el dominio espacial 2,9um(cuadrada)
-L = N*dx #Dimensiones del sensor
-
-l = 0.0001 #(diemnsiones de la apertura cuadrada) Usamos dimensión máxima: 1 cm
-M = int((l/L)*N)
-print(M)
+N = 800 #pixeles de la camara
+L = 10 #dimensiones del sensor
+dx = L/N #paso
 df = 1/L
 
+z = 5000
+z_max = N*(dx**2)/long_onda
+assert z <= z_max, "No cumple el criterio de z para TF"
+print(z_max)
+#########Coordenadas Espaciales
 
-##################  MÉTODO POR FUNCION DE TRANSFERENCIA EXACTA    ######################################################33
-
-
-#if M%2 == 0:
- #   N = 12*M
-#else:
-#    N = 12*M +1 
-     # Espaciado en el dominio de la frecuencia (cuadrada)
-     
-z = 0.1
-z_max = M*(dx**2)/long_de_onda
-print(z_max, M)
-#assert z <= z_max, "No cumple el criterio de z para TF"
-
-
-    
-abertura = np.ones((M,M), dtype=complex)
-padded_array = np.zeros((N, N), dtype=complex)
-min_index = (N-M)//2
-padded_array[min_index : min_index + M, min_index : min_index + M] = abertura
-
-# Discretizacion dominio espacial
-
-n = (np.arange(N) - N/2)
-m = (np.arange(N) - N/2)
+n = np.arange(N) - N//2
+m = np.arange(N) - N//2
 
 x = n*dx
 y = m*dx
+X,Y = np.meshgrid(x,y)
 
-X, Y = np.meshgrid(n, m)
+#######Espectro
 
-# Discretizacion espectro
- 
-p = (np.arange(N) - N/2) 
-q = (np.arange(N) - N/2) 
+p = np.arange(N) - N//2
+q = np.arange(N) - N//2
 
 fx = p*df
 fy = q*df
-P, Q = np.meshgrid(p, q)
+Fx,Fy = np.meshgrid(fx, fy)
+
+####Abertura Circular
+
+#r_0 = 2 # 2mm
+#abertura = (X**2 + Y**2) <= r_0**2
+
+#U_0 = abertura.astype(np.complex128)
+
+#Abertura Cuadrada
+#### Abertura Cuadrada
+lado = 0.5  # Longitud del lado del cuadrado en mm (ej: 4mm)
+abertura = (np.abs(X) <= lado / 2) & (np.abs(Y) <= lado / 2)
+U_0 = abertura.astype(np.complex128)
+
+####Hallemos A_0
+
+A_0 = np.fft.fft2(U_0) * (dx**2)
+A_0sh = np.fft.fftshift(A_0)
 
 
-k = 2*np.pi/long_de_onda #numero de onda
+####Hallemos A_0
 
-#Calculamos la matriz de fase cuadrática
+argumento_raiz = 1 - ((long_onda)**2 * (Fx**2 + Fy**2))
+A = A_0 * (np.exp(1j * z * k * np.sqrt(argumento_raiz)))
+A_shift = np.fft.ifftshift(A)
 
-#Multiplicación campo de entrada por la fase de entrada
-campo_en_apertura = padded_array     
+####Hallemos el campo de salida U
 
-f_max = M*df #Criterio de Aliasing
+U = (np.fft.ifft2(A_shift)) * ((df)**2)
+U_shift = np.fft.fftshift(U)
 
-#Transformada de Fourier
-difraccion_fft = np.fft.fft2(campo_en_apertura)
-centrar_fft = dx**2 * np.fft.fftshift(difraccion_fft)
+#####Hallemos la intensidad
 
-#Manejo ondas evanescentes
-argumento_raiz = 1 - (((long_de_onda*df)**2) * (P**2 + Q**2))
-print(1/long_de_onda)
-print(M*df)
-# Filtro para mantener solo las frecuencias que se propagan (argumento >= 0)
-filtro_propagacion = (argumento_raiz >= 0)
-
-# Inicializamos H con ceros (complejos)
-FuncionTransfer = np.zeros((N, N), dtype=complex)
-# Calculamos H solo para las frecuencias que se propagan
-FuncionTransfer[filtro_propagacion] = np.exp(1j*z*k*np.sqrt(argumento_raiz[filtro_propagacion]))
-
-
-A = centrar_fft * FuncionTransfer
-shift_A = np.fft.ifftshift(A)   #shift preparado para hacer la inversa
-CampoSalida = df**2 * np.fft.ifft2(shift_A) #inversa de fourier
-
-
-intensidad = abs(CampoSalida)**2
+intensidad = abs(U)**2
 max_intensidad = np.max(intensidad)
+intensidad_norm= intensidad/max_intensidad
+
 if max_intensidad > 0:
     intensidad_log = np.log1p(intensidad / max_intensidad * 100)
     intensidad_norm = intensidad_log / np.max(intensidad_log)
 else:
     intensidad_norm = intensidad
+
+####Grafiquemos
 
 #Aplicamos escala logarítmica (para visualizar detalles en zonas de baja intensidad)
 intensidad_log = np.log10(intensidad/max_intensidad + 1e-6)   #Se suma 1 a la intensidad para evitar log(0), que es -infinito
@@ -101,7 +85,7 @@ intensidad_log = np.log10(intensidad/max_intensidad + 1e-6)   #Se suma 1 a la in
 fig, ax = plt.subplots(1,2,figsize=(12,6))
 
 extent = [-L/2 * 1e3, L/2 * 1e3, -L/2 * 1e3, L/2 * 1e3]
-im0 = ax[0].imshow(np.abs(padded_array), cmap='gray', extent=extent)
+im0 = ax[0].imshow(np.abs(abertura), cmap='gray', extent=extent)
 ax[0].set_title("Plano de la Apertura", fontsize=14)
 ax[0].set_xlabel("x (mm)", fontsize=12)
 ax[0].set_ylabel("y (mm)", fontsize=12)
