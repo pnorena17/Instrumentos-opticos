8f1c80e2b54f007f2f4154820b06b7c71f94ff3d
pnorena17 <pnorena@unal.edu.co>
2025-09-22 22:38:40 -0500

    Implementación código FTE con fase

--- practica_1/punto4/Ing Inversa Fibra.py
+++ practica_1/punto4/Ing Inversa Fibra.py
@@ -1,24 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
+
+#Primero leemos la imagen en la ruta y la convierte en una matriz MxM
+ruta=r"C:\Users\pauli\OneDrive\Documents\Universidad\Instrumentos-opticos\practica_1\punto4\Resultados\fibra0_1.jpg"
+
+img = Image.open(ruta).convert("L") #la convertimos a blanco y negro
+arr = np.array(img)/255.0 #la normalizamos [0,1]
+N_size = np.shape((arr))
+
+if N_size[0] != N_size[1]:
+    N = min(N_size[0],N_size[1])
+    imagen = arr[int((-N+N_size[0])/2) : int((-N+N_size[0])/2) + N, int((-N+N_size[1])/2) : int((-N+N_size[1])/2) + N]
+    
+else:
+    N = N_size[0]
+    imagen = arr
+
+campo_detector = np.sqrt(imagen).astype(complex)
+
+#Tamaño del pixel del detector
+dx = 1.85e-6 # tamaño de pixel (1.85 um)
 
 #### Creamos las Variables
-
-long_onda = 633e-9 
+long_onda = 633e-9 #633 nm
 k = (2*np.pi)/long_onda
 
-
-N = 800 # pixeles de la camara
-L = 10 # dimensiones del sensor
-dx = L/N # tamaño de pixel
+L = dx*N # dimensiones del sensor
 df = 1/L # correspondiente en el espectro
 
-z = 50000 # distancia de la abertura al detector
+#Valores para ajustar
+z_fibra_a_detector = 0.03  # distancia de la fibra al detector (3 cm)
+z_fuente_a_detector = 0.08  #7 cm
 
 # Condiciones de buen muestreo
 
 z_max = N*(dx**2)/long_onda
-assert z <= z_max, "No cumple el criterio de z para TF"
 print(z_max)
+#assert z_fibra_a_detector <= z_max, "No cumple el criterio de z para FTE"
+
 
 ######### Coordenadas Espaciales
 
@@ -38,72 +58,51 @@ fx = p*df
 fy = q*df
 Fx,Fy = np.meshgrid(fx, fy)
 
+# Creamos la fase esférica de corrección. Esta es la fase que debemos "quitar" de la imagen capturada.
+fase_esferica_correccion = np.exp(-1j * k * (X**2 + Y**2) / (2 * z_fuente_a_detector))
 
-#### Abertura Circular
+# Corregimos el campo multiplicándolo por la fase de corrección
+campo_corregido = campo_detector * fase_esferica_correccion
 
-#r_0 = 1 # 2mm
-#abertura = (X**2 + Y**2) <= r_0**2
+#### Hallemos A_0 (Espectro Angular)
 
-#U_0 = abertura.astype(np.complex128)
-
-
-#### Abertura Cuadrada
-
-l = 2  # Longitud de la abertura
-abertura = (np.abs(X) <= l / 2) & (np.abs(Y) <= l / 2)
-U_0 = abertura.astype(np.complex128)
-
-#### Hallemos A_0
-
-A_0 = np.fft.fft2(U_0) * (dx**2)
+A_0 = np.fft.fft2(campo_corregido)
 A_0sh = np.fft.fftshift(A_0)
 
 
-#### Hallemos A
+#### Hallemos A (Propagación del Espectro Angular en el dominio espectral)
 
-argumento_raiz = 1 - ((long_onda)**2 * (Fx**2 + Fy**2))
-A = A_0 * (np.exp(1j * z * k * np.sqrt(argumento_raiz)))
-A_shift = np.fft.ifftshift(A)
+argumento_raiz = (2 * np.pi)**2 * ((1. / long_onda) ** 2 - Fx ** 2 - Fy ** 2)
+
+#Verificamos que usemos las ondas propagantes
+tmp = np.sqrt(np.abs(argumento_raiz))
+kz = np.where(argumento_raiz >= 0, tmp, 1j*tmp)
+
+A = A_0sh * (np.exp(-1j * z_fibra_a_detector * kz))
+A_ishift = np.fft.ifftshift(A)
 
 #### Hallemos el campo de salida U
 
-U = (np.fft.ifft2(A_shift)) * ((df)**2)
-U_shift = np.fft.fftshift(U)
+U = (np.fft.ifft2(A_ishift))
 
-##### Hallemos la intensidad
-
-intensidad = abs(U)**2
-max_intensidad = np.max(intensidad)
-intensidad_norm= intensidad/max_intensidad
-
-if max_intensidad > 0:
-    intensidad_log = np.log1p(intensidad / max_intensidad * 100)
-    intensidad_norm = intensidad_log / np.max(intensidad_log)
-else:
-    intensidad_norm = intensidad
 
 #### Grafiquemos
 
-# Aplicamos escala logarítmica (para visualizar detalles en zonas de baja intensidad)
-intensidad_log = np.log10(intensidad/max_intensidad + 1e-6)   #Se suma 1 a la intensidad para evitar log(0), que es -infinito
-
-fig, ax = plt.subplots(1,2,figsize=(12,6))
+fig, ax = plt.subplots(1,2,figsize=(10,6))
 
 extent = [-L/2 * 1e3, L/2 * 1e3, -L/2 * 1e3, L/2 * 1e3]
-im0 = ax[0].imshow(np.abs(abertura), cmap='gray', extent=extent)
+im0 = ax[0].imshow((np.abs(U)**2)*100, cmap='gray', extent=extent)
 ax[0].set_title("Plano de la Apertura", fontsize=14)
 ax[0].set_xlabel("x (mm)", fontsize=12)
 ax[0].set_ylabel("y (mm)", fontsize=12)
 ax[0].set_aspect('equal')
 
-im1 = ax[1].imshow(intensidad_norm, extent=extent, cmap="gray")
+extent = [-L/2 * 1e3, L/2 * 1e3, -L/2 * 1e3, L/2 * 1e3]
+im1 = ax[1].imshow(imagen, extent=extent, cmap="gray")
 ax[1].set_title("Patrón de Difracción")
 ax[1].set_xlabel("x en plano de observación (mm)")
 ax[1].set_ylabel("y en plano de observación (mm)")
 plt.colorbar(im1, ax=ax[1], label="Intensidad normalizada")
-
-center_index = N // 2
-perfil_intensidad = intensidad[center_index, :]
 
 fig.tight_layout()
 plt.show()
