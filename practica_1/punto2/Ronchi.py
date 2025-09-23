64d972f550bb364139d00932a2918630f8393921
pnorena17 <pnorena@unal.edu.co>
2025-09-22 19:04:59 -0500

    Correcciones rendija infinita

--- practica_1/punto2/Ronchi.py
+++ practica_1/punto2/Ronchi.py
@@ -1,23 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+##Primero generamos la rendija Ronchi
+#Parámetros
+M = 1080                     # Número de píxeles para la rendija
+lineas_mm = 10               # pares de líneas por mm
+fraccion_abierto = 0.5       # duty cycle
+
+#Escala física de la rejilla
+periodo_m = (1/lineas_mm) * 1e-3     # Periodo en metros (0.1 mm = 100 um)
+longitud_rejilla_m = 4e-3          # Lado físico de la rejilla en metros (1.5 mm)
+dx_0 = longitud_rejilla_m / M        # Tamaño de píxel de la rejilla en metros
+periodo_pix = periodo_m / dx_0
+
+#Creamos el espacio físico de la rejilla centrada en (N/2, N/2)
+n_0 = (np.arange(M) - M/2) * dx_0
+m_0 = (np.arange(M) - M/2) * dx_0
+N_0, M_0 = np.meshgrid(n_0, m_0)
+
+# Generar la rejilla 1D y expandirla a 2D
+rejilla_x = (np.mod(n_0 + periodo_m/2, periodo_m) < (fraccion_abierto * periodo_m)).astype(float)
+rejilla = np.tile(rejilla_x, (M, 1))
+
+
 #Definimos las variables con las que vamos a trabajar
 ##Variables ya establecidas 
-long_de_onda = 633e-9 #(en metros) Usamos la longitud de onda del  rojo: 633 nm
-N = 1200    #Resolución mínima de pixeles del detector DFM 37UX290-ML
-dx = 3e-6 #(en metros) Pixel size del detector (2.9 um)
+long_de_onda = 633e-9   #(en metros) Usamos la longitud de onda del  rojo: 633 nm
+N = M                   # La resolución debe ser la misma para una propagación correcta
 
 ##Variables modificables
-z = 0.0316     #(en metros) Distancia entre pantalla y abertura
+# Calculamos la distancia de Talbot correcta
+z_talbot = (2 * periodo_m**2) / long_de_onda
+z = 1*  z_talbot # Usamos la distancia de Talbot para ver la auto-imagen
 
-##Variables de la abertura
-#Por ejemplo, usaremos de abertura un cuadrado de lado l
-l = 1e-3    #(en metros) Usamos dimesión máxima (1 mm)
-dx_0 = long_de_onda*z/(N*dx)  #(en metros) Tamaño de pixel en nuestra abertura
-M = 2*int(l/dx_0)   #Muestreo de nuestra abertura
-L = N*dx_0
+print(z)
 
-print(M)
+#Calculamos tamaño de pixel en plano de difracción
+dx = long_de_onda*z/(N*dx_0)
 
 #Verificaciones antes de iniciar el cálculo
 z_min = M*(dx_0**2)/long_de_onda #(en metros) Distancia mínima de la pantalla para que podamos usar la Transformada de Fresnel
@@ -25,23 +44,14 @@ z_min = M*(dx_0**2)/long_de_onda #(en metros) Distancia mínima de la pantalla p
 
 ##################  MÉTODO POR TRANSFORMADA DE FRESNEL    ######################################################
 
-#Creamos el espacio físico de la rejilla infinita centrada en (N/2, N/2)
-n_0 = (np.arange(N) - N/2) * dx_0
-m_0 = (np.arange(N) - N/2) * dx_0
-N_0, M_0 = np.meshgrid(n_0, m_0)
+#Creamos la matriz NxN para la abertura
+iluminacion = np.ones((M,M), dtype=complex)                  #Esta es U[n_0,m_0,0]
+
+#Implementamos la Rejilla Ronchi
+transmitancia = rejilla.astype(complex) 
 
 #Creamos la matriz MxM para la abertura
-iluminacion = np.ones((N,N), dtype=complex)                  #Esta es U[n_0,m_0,0]
-
-#Creamos la Rejilla Ronchi
-lineas_mm = 10
-periodo = 1/(lineas_mm / 1e-3)
-fraccion_abierto = 0.5
-rejilla_x = (((N_0 % periodo) < (fraccion_abierto * periodo))).astype(float)
-transmitancia = rejilla_x.astype(complex) 
-
-#Modificamos la onda de entrada por la transmitancia encontrada
-campo_entrada = iluminacion * transmitancia
+campo_entrada = iluminacion*transmitancia                #Esta es U[n_0,m_0,0]
 
 #Calculamos la matriz de fase cuadrática
 k = 2*np.pi/long_de_onda
@@ -50,8 +60,13 @@ fase_cuadratica_entrada = np.exp(1j * (k / (2*z)) * ((N_0)**2 + ((M_0)**2)))
 #Multiplicación campo de entrada por la fase de entrada
 campo_en_apertura = campo_entrada * fase_cuadratica_entrada    #Este es U'[n_0,m_0,0]
 
+#Hacemos la operación para rellenar de 0 la matriz NxN por fuera de la MxM
+matriz_con_relleno = np.zeros((N, N), dtype=complex)
+min_indice = (N-M)//2
+matriz_con_relleno[min_indice : min_indice + M, min_indice : min_indice + M] = campo_en_apertura
+
 #Ahora, debemos realizar la transformada de Fourier de 2 dimensiones
-difraccion_fft = np.fft.fft2(campo_en_apertura)
+difraccion_fft = np.fft.fft2(matriz_con_relleno)
 centrar_fft = dx**2 * np.fft.fftshift(difraccion_fft)          #Esta es U"[n,m,z]
 
 #Creamos el espacio físico de la pantalla centrado en (N/2,N/2)
@@ -76,12 +91,13 @@ else:
 #Aplicamos escala logarítmica (para visualizar detalles en zonas de baja intensidad)
 intensidad_log = np.log10(intensidad/max_intensidad + 1e-6)   #Se suma 1 a la intensidad para evitar log(0), que es -infinito
 
+#intensidad_norm = intensidad/max_intensidad
 
 #Graficamos
 fig, ax = plt.subplots(1,2,figsize=(10,5))
 
-extent = [-L/2, L/2, -L/2, L/2]
-im0 = ax[0].imshow(np.abs(campo_en_apertura), cmap='gray', extent=extent)
+extent = [-longitud_rejilla_m/2, longitud_rejilla_m/2, -longitud_rejilla_m/2, longitud_rejilla_m/2]
+im0 = ax[0].imshow(np.abs(matriz_con_relleno), cmap='gray', extent=extent)
 ax[0].set_title("Plano de Difracción")
 ax[0].set_xlabel("x en plano de difracción (m)")
 ax[0].set_ylabel("y en plano de difracción (m)")
@@ -90,7 +106,7 @@ ax[0].set_aspect('equal')
 
 extent = [m.min(), m.max(), n.min(), n.max()]
 im = ax[1].imshow(intensidad_norm, extent=extent, cmap="gray")
-ax[1].set_title("Patrón de Difracción")
+ax[1].set_title(f"Patrón de Difracción a z = {z*1e3:.2f} mm")
 ax[1].set_xlabel("x en plano de observación (m)")
 ax[1].set_ylabel("y en plano de observación (m)")
 
@@ -98,3 +114,5 @@ plt.colorbar(im, ax=ax[1], label="Intensidad normalizada")
 # Mostrar gráficos
 plt.tight_layout()
 plt.show()
+
+
