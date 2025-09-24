b37f9c913a7517f6353e98d5f5e95c46953c158e
pnorena17 <pnorena@unal.edu.co>
2025-09-24 17:17:25 -0500

    Implementación zero padding

--- practica_1/punto4/Ing Inversa Fibra.py
+++ practica_1/punto4/Ing Inversa Fibra.py
@@ -3,18 +3,19 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 #Primero leemos la imagen en la ruta y la convierte en una matriz MxM
-ruta=r"C:\Users\pauli\OneDrive\Documents\Universidad\Instrumentos-opticos\practica_1\punto4\Resultados\fibra0_1.jpg"
+ruta=r"C:\Users\pauli\OneDrive\Documents\Universidad\Instrumentos-opticos\practica_1\punto4\Resultados\fibra0_0.jpg"
 
 img = Image.open(ruta).convert("L") #la convertimos a blanco y negro
 arr = np.array(img)/255.0 #la normalizamos [0,1]
-N_size = np.shape((arr))
+M_size = np.shape((arr))
 
-if N_size[0] != N_size[1]:
-    N = min(N_size[0],N_size[1])
-    imagen = arr[int((-N+N_size[0])/2) : int((-N+N_size[0])/2) + N, int((-N+N_size[1])/2) : int((-N+N_size[1])/2) + N]
+if M_size[0] != M_size[1]:
+    M = max(M_size[0],M_size[1])
+    imagen = np.zeros((M,M)) 
+    imagen[int((M-M_size[0])/2) : int((M-M_size[0])/2) + M_size[0], int((M-M_size[1])/2) : int((M-M_size[1])/2) + M_size[1]] = arr
     
 else:
-    N = N_size[0]
+    M = M_size[0]
     imagen = arr
 
 campo_detector = np.sqrt(imagen).astype(complex)
@@ -22,8 +23,15 @@ campo_detector = np.sqrt(imagen).astype(complex)
 #Tamaño del pixel del detector
 dx = 1.85e-6 # tamaño de pixel (1.85 um)
 
+N = 2**13
+
+#Hacemos la operación para rellenar de 0 la matriz NxN por fuera de la MxM
+matriz_con_relleno = np.zeros((N, N), dtype=complex)
+min_indice = (N-M)//2
+matriz_con_relleno[min_indice : min_indice + M, min_indice : min_indice + M] = campo_detector
+
 #### Creamos las Variables
-long_onda = 633e-9 #633 nm
+long_onda = 632.8e-9 #633 nm
 k = (2*np.pi)/long_onda
 
 L = dx*N # dimensiones del sensor
@@ -31,7 +39,7 @@ df = 1/L # correspondiente en el espectro
 
 #Valores para ajustar
 z_fibra_a_detector = 0.03  # distancia de la fibra al detector (3 cm)
-z_fuente_a_detector = 0.08  #7 cm
+z_fuente_a_detector = 0.090  #7 cm
 
 # Condiciones de buen muestreo
 
@@ -62,7 +70,7 @@ Fx,Fy = np.meshgrid(fx, fy)
 fase_esferica_correccion = np.exp(-1j * k * (X**2 + Y**2) / (2 * z_fuente_a_detector))
 
 # Corregimos el campo multiplicándolo por la fase de corrección
-campo_corregido = campo_detector * fase_esferica_correccion
+campo_corregido = matriz_con_relleno * fase_esferica_correccion
 
 #### Hallemos A_0 (Espectro Angular)
 
@@ -72,7 +80,7 @@ A_0sh = np.fft.fftshift(A_0)
 
 #### Hallemos A (Propagación del Espectro Angular en el dominio espectral)
 
-argumento_raiz = (2 * np.pi)**2 * ((1. / long_onda) ** 2 - Fx ** 2 - Fy ** 2)
+argumento_raiz = (2 * np.pi)**2 * ((1. / long_onda)**2 - Fx** 2 - Fy** 2)
 
 #Verificamos que usemos las ondas propagantes
 tmp = np.sqrt(np.abs(argumento_raiz))
@@ -85,20 +93,21 @@ A_ishift = np.fft.ifftshift(A)
 
 U = (np.fft.ifft2(A_ishift))
 
+fase_esferica_correccion = np.exp(-1j * k * (X**2 + Y**2) / (2 * (z_fuente_a_detector-z_fibra_a_detector)))
 
 #### Grafiquemos
 
 fig, ax = plt.subplots(1,2,figsize=(10,6))
 
 extent = [-L/2 * 1e3, L/2 * 1e3, -L/2 * 1e3, L/2 * 1e3]
-im0 = ax[0].imshow((np.abs(U)**2)*100, cmap='gray', extent=extent)
+im0 = ax[0].imshow(np.abs(U*fase_esferica_correccion)**2, cmap='gray', extent=extent)
 ax[0].set_title("Plano de la Apertura", fontsize=14)
 ax[0].set_xlabel("x (mm)", fontsize=12)
 ax[0].set_ylabel("y (mm)", fontsize=12)
 ax[0].set_aspect('equal')
 
 extent = [-L/2 * 1e3, L/2 * 1e3, -L/2 * 1e3, L/2 * 1e3]
-im1 = ax[1].imshow(imagen, extent=extent, cmap="gray")
+im1 = ax[1].imshow(np.abs(matriz_con_relleno), extent=extent, cmap="gray")
 ax[1].set_title("Patrón de Difracción")
 ax[1].set_xlabel("x en plano de observación (mm)")
 ax[1].set_ylabel("y en plano de observación (mm)")
