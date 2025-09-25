5fe029cbc3ce09a4a23dcee856d8494f985e9880
LucasOsorno <losornoo@unal.edu.co>
2025-09-25 09:56:14 -0500

    ajuste reescala de la imagen de entrada

--- practica_1/punto4/Ing Inversa Fibra.py
+++ practica_1/punto4/Ing Inversa Fibra.py
@@ -3,10 +3,28 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 #Primero leemos la imagen en la ruta y la convierte en una matriz MxM
-ruta=r"C:\Users\pauli\OneDrive\Documents\Universidad\Instrumentos-opticos\practica_1\punto4\Resultados\fibra0_0.jpg"
+ruta=r"C:\Users\david\OneDrive\Desktop\Universidad\Semestre 11\Instrumentos Opticos\Instrumentos-opticos\practica_1\punto4\Resultados\fibra0_0.jpg"
 
-img = Image.open(ruta).convert("L") #la convertimos a blanco y negro
-arr = np.array(img)/255.0 #la normalizamos [0,1]
+img = Image.open(ruta).convert("L") #la convertimos a blanco y negro, Objeto Image (4000x3000)
+
+ancho_original, alto_original = img.size # (4000, 3000)
+
+# 1. Definir la nueva dimensión deseada (ej. el lado más largo debe ser 1080)
+M = 1080 
+
+# 2. Calcular el factor de escala
+# Usamos el lado más grande (4000) para calcular el factor de escala
+factor_escala = M / max(ancho_original, alto_original) 
+
+# 3. Calcular las nuevas dimensiones manteniendo la proporción
+nuevo_ancho = int(ancho_original * factor_escala) 
+nuevo_alto = int(alto_original * factor_escala)
+
+# Las nuevas dimensiones serán (1080, 810)
+# 4. Aplicar el resize
+arr = img.resize((nuevo_ancho, nuevo_alto))
+arr = np.array(arr)/255.0 #la normalizamos [0,1]
+
 M_size = np.shape((arr))
 
 if M_size[0] != M_size[1]:
@@ -18,12 +36,13 @@ else:
     M = M_size[0]
     imagen = arr
 
+
 campo_detector = np.sqrt(imagen).astype(complex)
 
 #Tamaño del pixel del detector
 dx = 1.85e-6 # tamaño de pixel (1.85 um)
 
-N = 2**13
+N = 2048
 
 #Hacemos la operación para rellenar de 0 la matriz NxN por fuera de la MxM
 matriz_con_relleno = np.zeros((N, N), dtype=complex)
@@ -107,7 +126,7 @@ ax[0].set_ylabel("y (mm)", fontsize=12)
 ax[0].set_aspect('equal')
 
 extent = [-L/2 * 1e3, L/2 * 1e3, -L/2 * 1e3, L/2 * 1e3]
-im1 = ax[1].imshow(np.abs(matriz_con_relleno), extent=extent, cmap="gray")
+im1 = ax[1].imshow(imagen, extent=extent, cmap="gray")
 ax[1].set_title("Patrón de Difracción")
 ax[1].set_xlabel("x en plano de observación (mm)")
 ax[1].set_ylabel("y en plano de observación (mm)")
