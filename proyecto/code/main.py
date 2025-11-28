82a2289820be8ffd95eb53b27031a7c17941a31b
pnorena17 <pnorena@unal.edu.co>
2025-11-28 10:58:34 -0500

    Archivo donde se correrá el código

---
+++ proyecto/code/main.py
@@ -0,0 +1,7 @@
+import numpy as np
+from gif_to_frame import extraer_frames
+
+
+#Creamos la lista con los frames extraidos del gif
+archivo_gif = r"C:\Users\pauli\OneDrive\Documents\Universidad\Instrumentos-opticos\proyecto\video_test\SpAQ.gif" 
+lista_frames = extraer_frames(archivo_gif)
