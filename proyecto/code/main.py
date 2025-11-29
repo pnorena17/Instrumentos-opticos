88090dd60bea3493753a0ac67d885efcdb7bd78b
pnorena17 <pnorena@unal.edu.co>
2025-11-28 22:33:33 -0500

    pruebas con generacion de qr

--- proyecto/code/main.py
+++ proyecto/code/main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from gif_to_frame import extraer_frames
 from encript_image import encriptar_drpe
 from encript_image import desencriptar_drpe
+import generate_qr as gqr
 
 ## Extraemos el video a procesar
 # Ruta del archivo
@@ -48,3 +49,14 @@ if len(lista_frames) > 0:
 
     plt.tight_layout()
     plt.show()
+
+matriz = gqr.generar_qr_matriz(frame)
+
+"""# 2. Lo visualizamos
+plt.figure(figsize=(6, 6))
+# cmap='binary': 0 lo pinta blanco, 1 lo pinta negro (Estándar QR)
+# vmin=0, vmax=1: Asegura que el contraste sea total
+plt.imshow(matriz, cmap='binary', vmin=0, vmax=1) 
+plt.title(f"Código QR ({matriz.shape[0]}x{matriz.shape[1]})")
+plt.axis('off') # Quitamos los números de los ejes para que se vea limpio
+plt.show()"""
