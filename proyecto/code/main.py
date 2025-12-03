a12788eadc172abfac2a0aa1f4b342d0049f7a47
pnorena17 <pnorena@unal.edu.co>
2025-12-03 10:24:46 -0500

    Implmentación función encript qr

--- proyecto/code/main.py
+++ proyecto/code/main.py
@@ -1,10 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.ndimage import median_filter
 from gif_to_frame import extraer_frames
 from encript_image import encriptar_drpe
 from encript_image import desencriptar_drpe
 import generate_qr as gqr
 import reconstruccion as rqr
+from qr_basic import encriptacion_imagen_qr
+from play_gif import reproducir_gif
+
 #import multiplexing as mux 
 
 ## Extraemos el video a procesar
@@ -14,95 +18,23 @@ archivo_gif = r"C:\Users\pauli\OneDrive\Documents\Universidad\Instrumentos-optic
 # Creamos la lista con los frames extraidos del gif
 lista_frames = extraer_frames(archivo_gif)
 
-FILAS = 6
-COLS = 6
-
+FILAS = 10
+COLS = 10
+RADIO_PUPILA = 0.65 #Proporcion
 
 if len(lista_frames) > 0:
-    # Vamos a trabajar solo con el primer frame para probar
-    frame = lista_frames[0]
-    
-    #Generamos la matriz con los QR de cada frame
-    lista_qr = gqr.generar_lista_qrs(frame, filas=FILAS, cols=COLS, escala=2)
 
-    matriz_qr = lista_qr[0]
+    gif_recuperado = []
 
-    # Llamamos a la función de encriptar
-    #radio = 400 #pixeles
-    #img_cifrada, k1, k2, mascara_pupila = encriptar_drpe(frame, radio_pupila=radio)
-    matriz_cifrada, k1, k2, mascara_pupila = encriptar_drpe(matriz_qr, radio_pupila=100)
-    
-    
-    # Probamos desencriptar para ver si recuperamos la imagen
-    #img_recuperada = desencriptar_drpe(img_cifrada, k1, k2)
-    matriz_recuperada = desencriptar_drpe(matriz_cifrada, k1, k2)
-    matriz_recuperada_ruidosa = np.abs(matriz_recuperada)
-    
-    # Limpieza (son una prueba para lograr disminuir el ruido y por ende el radio)
-    
-    #matriz_norm = (matriz_recuperada_ruidosa - matriz_recuperada_ruidosa.min())/(matriz_recuperada_ruidosa.max() - matriz_recuperada_ruidosa.min())
-    #matriz_limpia = np.where(matriz_norm > 0.5, 1, 0)
-    
-    # Verificación matemática rápida
-    # Restamos la original de la recuperada.
-    #error = np.mean((frame - img_recuperada)**2)
-    error = np.mean((matriz_qr - matriz_recuperada_ruidosa)**2)
-    #print(f"Error cuadrático medio (MSE): {error:.5f}")
-    print(f"Error cuadrático medio (MSE): {error:.5f}")
+    for i, frame in enumerate(lista_frames):
+        # Vamos a trabajar solo con el primer frame para probar
+        print(f"Encripatando el frame {i+1}")
+        frame_recuperado = encriptacion_imagen_qr(frame, FILAS, COLS, RADIO_PUPILA, graph=False)
 
-    # 4. Graficar
-    plt.figure(figsize=(12, 4))
+        gif_recuperado.append(frame_recuperado)
 
-    # Imagen Original
-    plt.subplot(1, 3, 1)
-    plt.imshow(frame, cmap='gray')
-    plt.title("Original")
-    plt.colorbar(fraction=0.046, pad=0.04)
+    reproducir_gif(gif_recuperado)
 
-    # Imagen Encriptada 
-    plt.subplot(1, 3, 2)
-    #plt.imshow(np.abs(img_cifrada), cmap='gray')
-    plt.imshow(np.abs(matriz_cifrada), cmap='gray')
-    plt.title("Encriptada (Ruido)")
-    plt.colorbar(fraction=0.046, pad=0.04)
-
-    # Imagen Recuperada
-    plt.subplot(1, 3, 3)
-    #plt.imshow(img_recuperada, cmap='gray')
-    plt.imshow(matriz_recuperada_ruidosa, cmap='gray')
-    plt.title("Recuperada")
-    plt.colorbar(fraction=0.046, pad=0.04)
-
-    plt.tight_layout()
-    plt.show()
-
-
-#FILAS = 6
-#COLS = 8
-
-#matriz = gqr.generar_mosaico_raw(frame, filas=FILAS, cols=COLS, escala=2)
-
-# Lo visualizamos
-
-plt.figure(figsize=(6, 6))
-plt.imshow(matriz_qr, cmap='binary', vmin=0, vmax=1)  # vmin=0, vmax=1: Asegura que el contraste sea total
-plt.title(f"Código QR ({matriz_recuperada.shape[0]}x{matriz_recuperada.shape[1]})")
-plt.axis('off') # Quitamos los números de los ejes para que se vea limpio
-plt.show()
-
-imagen_final = rqr.leer_qr_individual(matriz_recuperada_ruidosa)
-
-# 3. Visualizar CON PROTECCIÓN
-if imagen_final is not None:
-    plt.figure(figsize=(10, 10))
-    # cmap='binary': 0 es blanco, 1 es negro.
-    # Si tus QRs salen invertidos (negro donde debe ser blanco), cambia a cmap='gray' o invierte (1 - mosaico)
-    plt.imshow(imagen_final, cmap='gray', vmin=0, vmax=1)
-    plt.title(f"Mosaico Generado ({FILAS}x{COLS})")
-    plt.axis('off')
-    plt.show()
-else:
-    print("No se pudo graficar porque falló la generación de los QRs.")
 
 """
 # Prueba Multipelxing
